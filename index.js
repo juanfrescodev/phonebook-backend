8f66a6c9713563421d2b9142e9351ac9bc8d15f4
Juan Fresco <juanfresco1@gmail.com>
2025-09-03 15:45:13 -0300

    Ejercicios 3.15-3.18: eliminación en MongoDB, middleware de errores, actualización con PUT y rutas con DB

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const morgan = require('morgan')
 const cors = require('cors')
 const path = require('path')
 const Person = require('./models/person')
+
 const app = express()
-  app.use(express.json())
+
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (req) => {
   return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(cors())
-app.use(express.static('dist'))
-
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons =>{
@@ -31,17 +32,24 @@ app.get('/info', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
-    response.json(person)
-})
+    if (person){
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then(() => {
-    response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+      response.status(204).end()
   })
-}) 
+  .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -66,9 +74,41 @@ app.post('/api/persons', (request, response) => {
 })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
